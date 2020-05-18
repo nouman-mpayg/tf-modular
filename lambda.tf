c382abf97206ff8d11c66fc5871f61f04d3ac7d4
Nouman Dilshad <adeel@Noumans-Mac.local>
2020-05-18 12:06:19 +0500

    region name issue fixed

--- lambda.tf
+++ lambda.tf
@@ -2,7 +2,7 @@ module "lambda_function_invoke_01" {
   source     = "./modules/lambda"
   func_name  = "tf_lambda_function_01"
   source_dir = "${path.root}/data/raw-lambdas/tf_lambda_function_01"
-  region     = "u-west-1"
+  region     = "eu-west-1"
   env = {
     PYTHONPATH = "/var/task/vendors:/var/runtime"
   }
