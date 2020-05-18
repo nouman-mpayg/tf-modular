e05e89f4f18f429950ac10777490bb9937e6be54
Nouman Dilshad <adeel@Noumans-Mac.local>
2020-05-18 12:22:35 +0500

    gitignore added

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -103,10 +103,11 @@ resource "aws_lambda_function" "lambda_function" {
   function_name = "${var.func_name}"
   role          = "${aws_iam_role.iam_for_lambda.arn}"
   handler       = "${var.func_name}.handler"
+  source_code_hash = "${data.archive_file.make_zip.output_base64sha256}"
   runtime       = "${var.runtime}"
   timeout       = "${var.timeout}"
   depends_on = [
-      "archive_file.make_zip",
+      "aws_iam_role_policy.lambda_invoke_sfn_policy",
     ]
   environment = {
     variables = "${var.env}"
