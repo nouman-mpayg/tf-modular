f9b5db983e8ace157e420998cc065e36548e6081
Nouman Dilshad <adeel@Noumans-Mac.local>
2020-05-18 10:04:42 +0500

    root path changed

--- modules/step-function/variable.tf
+++ modules/step-function/variable.tf
@@ -1,5 +1,5 @@
 data "template_file" "sfn_file" {
-  template = "${file("*/services/data/step-function-jsons/${var.sfn_name}.json")}"
+  template = "${file("${path.root}/data/step-function-jsons/${var.sfn_name}.json")}"
 
   vars = {
     region                  = "${var.region}"
